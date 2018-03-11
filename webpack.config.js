0858dc9312be549279c539f3c42fd917565f354b
Mats Andresen <mandresen@papego.de>
2018-03-11 18:01:41 +0100

    enable code-splitting

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ const webpack = require('webpack')
 const CleanWebpackPlugin = require("clean-webpack-plugin")
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const UglifyWebpackPlugin = require("uglifyjs-webpack-plugin")
+
+const isProductionMode = process.env.NODE_ENV === "production"
 
 const sourceDir = "src"
 const distributionDir = "dist"
@@ -20,7 +23,7 @@ module.exports = {
         filename: "[name].bundle.js",
         publicPath: "/"
     },
-    devtool: "inline-source-map",
+    devtool: "#sourcemap",
     module: {
         rules: [
             {
@@ -48,11 +51,38 @@ module.exports = {
     resolve: {
         extensions: [ ".ts", ".js" ]
     },
-    plugins: [
+    optimization: {
+        splitChunks: {
+            name: "vendor",
+            filename: "assets/vendor.js"
+        }
+    },
+    plugins: getPlugins()
+}
+
+function getPlugins() {
+    const generalPlugins = [
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+        }),
         new CleanWebpackPlugin([distributionDir]),
         new ExtractTextPlugin("app.bundle.css"),
         new HtmlWebpackPlugin({
             template: "index.html"
         })
     ]
-};
+    const developmentPlugins = []
+    const productionPlugins = [
+        new UglifyWebpackPlugin({
+            sourceMap: true,
+            uglifyOptions: {
+                output: {
+                    semicolons: false,
+                }
+            }
+        })
+    ]
+    return isProductionMode 
+        ? generalPlugins.concat(productionPlugins)
+        : generalPlugins.concat(developmentPlugins)
+}
