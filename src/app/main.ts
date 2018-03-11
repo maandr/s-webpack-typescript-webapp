64bb0c1b95eeb3df46cfb145ef9e8808e3d177d7
Mats Andresen <mandresen@papego.de>
2018-03-11 14:31:58 +0100

    webpack, ts, sass, images, html bundleing

---
+++ src/app/main.ts
@@ -0,0 +1,11 @@
+import "../assets/sass/main.scss"
+
+import * as Math from "./lib/math"
+
+const x = 20
+const y = 10
+
+console.log(x + " + " + y + " = " + Math.add(x, y))
+console.log(x + " - " + y + " = " + Math.subtract(x, y))
+console.log(x + " * " + y + " = " + Math.multiply(x, y))
+console.log(x + " / " + y + " = " + Math.divide(x, y))
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const webpack = require('webpack')
+
+// plugins
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+const sourceDir = "src"
+const distributionDir = "dist"
+
+module.exports = {
+    context: path.resolve(__dirname, sourceDir),
+    entry: {
+        app: './app/main.ts',
+        "math.lib": "./app/lib/math.ts"
+    },
+    output: {
+        path: path.resolve(__dirname, distributionDir),
+        filename: '[name].bundle.js'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(s*)css$/,
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    fallback: "style-loader"
+                }) 
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                use: [ "file-loader" ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ ".ts", ".js" ]
+    },
+    plugins: [
+        new CleanWebpackPlugin([distributionDir]),
+        new ExtractTextPlugin("app.bundle.css"),
+        new HtmlWebpackPlugin({
+            template: "index.html"
+        })
+    ]
+};
